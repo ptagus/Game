343364e0ef38befdd5c0d2d460dd55c0c6de774f
ptagus <ptagusss@ya.ru>
2021-04-25 13:43:00 +0300

    mining

--- Assets/Scripts/MapPoint.cs
+++ Assets/Scripts/MapPoint.cs
@@ -5,5 +5,25 @@ using UnityEngine;
 public class MapPoint : MonoBehaviour
 {
     public int[] links;
+    public bool explored;
+    public bool onExplore;
+    public int workershere;
+    public int number;
+    public int goldCountMin, goldCountMax;
+    public int gold;
+
+    private void Awake()    
+    {
+        gold = HowMuchGold();
+    }
+    public int HowMuchGold()
+    {
+        return Random.Range(goldCountMin, goldCountMax) / 3 * 3;
+    }
+
+    public void GoldCheck(int goldmine)
+    {
+        gold -= workershere * goldmine;
+    }
 }
 
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
+    [Header("Map")]
+    public GameObject[] maps;
+    public MapPoint[] mappoints;
+    int[] points;
+    int mappos;
     [Header("Gold")]
     public int gold;
     public int goldmining;
@@ -13,22 +18,36 @@ public class Test : MonoBehaviour
     public int goldminingfactor;
     public int upkeepfactor;
     public int goldincreas;
+    public int escortfactor;
     [Header("People")]
     public int workers;
     public int warriors;
+    int freeworkers;
     [Header("Costs")]
     public int workercost;
     public int warriorcost;
     [Header("Windows")]
     public GameObject buyWindow;
+    public GameObject exploreWindow;
+    public GameObject mineWindow;
+    public GameObject sendWorkersWindow;
     public Slider counter;
+    public Slider counterworkers;
     public Text counterlabel;
+    public Text counterworkerslabel;
+    [Header("ResoursesText")]
+    public Text goldText, workerstext, warriortext;
     int nowtype;
-    int tempcost = 0, tempupkeep = 0, tempfullcost = 0, tempcount = 0;
+    int tempcost = 0, tempupkeep = 0, tempfullcost = 0, tempcount = 0, tempworkers = 0, tempmappoint = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        freeworkers = workers;
+        int count = maps.Length;
+        points = new int[count];
+        goldText.text = gold.ToString();
+        workerstext.text = workers.ToString();
+        warriortext.text = warriors.ToString();
     }
 
     // Update is called once per frame
@@ -40,6 +59,7 @@ public class Test : MonoBehaviour
     public void BuyWorker(int count, int cost)
     {
         workers += count;
+        freeworkers += count;
         gold -= cost;
     }
 
@@ -62,11 +82,19 @@ public class Test : MonoBehaviour
     {
         warriors -= count;
         upkeep -= count * upkeepfactor;
+        UpdateText();
+    }
+
+    public void LoseWorkers(int count)
+    {
+        workers -= count;
+        UpdateText();
     }
 
     public void GoldIncrease(int sum)
     {
         goldincreas += sum;
+        UpdateText();
     }
 
     public void BuyWindow(int type)
@@ -114,8 +142,8 @@ public class Test : MonoBehaviour
         {
             BuyWarrior(tempcount, tempfullcost, tempupkeep);
         }
-        counter.value = 0;
-        buyWindow.SetActive(false);
+        DenyBuy();
+        UpdateText();
     }
 
     public void DenyBuy()
@@ -126,5 +154,156 @@ public class Test : MonoBehaviour
         tempcount = 0;
         counter.value = 0;
         buyWindow.SetActive(false);
+    }
+
+    public void UpdateText()
+    {
+        goldText.text = gold.ToString();
+        workerstext.text = workers.ToString();
+        warriortext.text = warriors.ToString();
+    }
+
+    public void ShowExploreWindow(MapPoint mp)
+    {
+        if (mp.onExplore)
+        {
+            Debug.LogError("OnExplore");
+            return;
+        }
+        if (mp.explored)
+        {
+            NowMapPoint(mp.number);
+            mineWindow.SetActive(true);
+            counterworkers.maxValue = 5 - HowMuckWorkersOnPoint();
+            return;
+        }
+        NowMapPoint(mp.number);
+        exploreWindow.SetActive(true);
+    }
+
+    public void CloseExploreWindow()
+    {
+        exploreWindow.SetActive(false);
+    }
+
+    public void CloseMineWindow()
+    {
+        mineWindow.SetActive(false);
+    }
+
+    public void Explore()
+    {
+        OpenPoint(mappoints[tempmappoint]);
+    }
+    public void ExploreEscort()
+    {
+        if (gold > escortfactor)
+        {
+            gold -= escortfactor;
+            OpenPoint(mappoints[tempmappoint]);
+        }
+        else
+        {
+            Debug.Log("Not Enogth money");
+            return;
+        }
+    }
+
+    public void OpenPoint(MapPoint mp)
+    {
+        for (int i = 0; i < mp.links.Length; i++)
+        {
+            points[mappos] = mp.links[i];
+            mappos++;
+        }
+        mp.onExplore = true;
+        CloseExploreWindow();
+    }
+
+    public void EndTurn()
+    {
+        foreach (MapPoint m in mappoints)
+        {           
+            if (m.workershere > 0)
+            {
+                m.GoldCheck(goldminingfactor);
+                gold+=goldmining;
+                goldmining = 0;
+                UpdateText();
+                m.workershere = 0;
+                freeworkers = workers;
+            }
+            if (m.onExplore)
+                m.explored = true;
+            m.onExplore = false;
+        }
+        for (int i = 0; i < points.Length; i++)
+        {
+            maps[points[i]].SetActive(true);
+        }
+    }
+
+    void NowMapPoint(int number)
+    {
+        tempmappoint = number;
+    }
+
+    public void SendWorkers()
+    {  
+        if (HowMuckGoldOnPoint() / 3 < 5)
+        {
+            counterworkers.maxValue = HowMuckGoldOnPoint() / 3;
+            tempworkers = (int)counterworkers.value;
+            counterworkerslabel.text = "Count: " + tempworkers.ToString();
+            return;
+        }
+        if (freeworkers > 5 - HowMuckWorkersOnPoint())
+        {
+            counterworkers.maxValue = 5 - HowMuckWorkersOnPoint();
+        }
+        else
+            counterworkers.maxValue = freeworkers;
+        tempworkers = (int)counterworkers.value;
+        counterworkerslabel.text = "Count: " + tempworkers.ToString();
+    }
+
+    public void ApplySend()
+    {
+        mappoints[tempmappoint].workershere += tempworkers;
+        freeworkers -= tempworkers;
+        goldmining += tempworkers * goldminingfactor;
+        counterworkers.value = 0;
+        tempworkers = 0;
+    }
+
+    public void DenySend()
+    {
+        counterworkers.value = 0;
+        tempworkers = 0;
+    }
+
+    int HowMuckWorkersOnPoint() 
+    {
+        return mappoints[tempmappoint].workershere;
+    }
+
+    int HowMuckGoldOnPoint()
+    {
+        Debug.LogError(mappoints[tempmappoint].gold);
+        return mappoints[tempmappoint].gold;
+    }
+    public void OpenSendWorkersWindow()
+    {
+        if (HowMuckWorkersOnPoint() == 5)
+        {
+            Debug.LogError("FULLhere");
+            return;
+        }
+        sendWorkersWindow.SetActive(true);
+    }
+
+    public void CheckGoldInPoint()
+    {
+
     }
 }
